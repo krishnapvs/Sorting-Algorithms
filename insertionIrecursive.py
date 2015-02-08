57f9b501c49e7ebc74073abb9e1e85af0a771183
Krishna VS Puvvada <vpuvvad@ncsu.edu>
2015-02-08 13:47:49 -0500

    Update insertionIrecursive.py

--- insertionIrecursive.py
+++ insertionIrecursive.py
@@ -1,4 +1,17 @@
 # insertion recursive
 
-def insertionSortRecursive(A):
-	
+def insertionSortRecursive(A,n):
+	if n>1:
+		insertionSortRecursive(A,n-1)
+		key=A[n-1]
+		i=A[n-2]
+		while i>=0 and key<A[i]:
+			A[i+1]=A[i]
+			i-=1
+		A[i+1]=key
+	return A
+
+
+A=[9.8,0,3,33,-1,-234,-34]
+
+print insertionSortRecursive(A,len(A))
